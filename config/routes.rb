2378108c155507f9dcd5e5ce3c05e38d6ff34c33
Julio Caminha <juliocaminha20@gmail.com>
2015-04-23 01:16:11 -0300

    Configurando o devise para autenticacao do usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root :to => redirect('/posts')
   resources :posts
+
+  authenticate :user do
+    resources :post, only: [:new, :create]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
