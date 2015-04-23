5ce5fa87dae0274470ed182c8464a7c9691ed203
Julio Caminha <juliocaminha20@gmail.com>
2015-04-23 04:31:39 -0300

    Refatorando testes de controller - Posts

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -23,13 +23,9 @@ RSpec.describe PostsController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Post. As you add validations to Post, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) { { "author" => "John Doe", "text" => "Mussum ipsum cacilds"}}
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) { { "author" => nil, "text" => "Mussum ipsum cacilds"}}
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
@@ -102,15 +98,13 @@ RSpec.describe PostsController, type: :controller do
 
   describe "PUT #update" do
     context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      let(:new_attributes) { { "author" => nil , "text" => "Mussum ipsum cacilds"}}
 
       it "updates the requested post" do
         post = Post.create! valid_attributes
         put :update, {:id => post.to_param, :post => new_attributes}, valid_session
         post.reload
-        skip("Add assertions for updated state")
+        expect(assigns(:post).attributes.symbolize_keys[:author]).to eq(new_attributes[:author])
       end
 
       it "assigns the requested post as @post" do
